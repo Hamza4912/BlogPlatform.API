0578f05ff1f5bc26939da6b85df160a0db5e38f1
Hamza4912 <hamzajamil1046@gmail.com>
2026-07-29 18:03:47 +0500

    Implement user registration and JWT generation

---
+++ BlogPlatform.API/Services/AuthService.cs
@@ -0,0 +1,89 @@
+﻿using BlogPlatform.API.Data;
+using BlogPlatform.API.DTO.Auth;
+using BlogPlatform.API.Models;
+using BlogPlatform.API.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BlogPlatform.API.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(AppDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        public async Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto)
+        {
+            bool emailExists = await _context.Users.AnyAsync(u => u.Email == registerDto.Email);
+
+            if (emailExists)
+            {
+
+                throw new Exception("Email is already registered.");
+            }
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(registerDto.Password);
+            var user = new User
+            {
+                Name = registerDto.Username,
+                Email = registerDto.Email,
+                PasswordHash = hashedPassword,
+                Role = "User",
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthResponseDto> LoginAsync(LoginDto loginDto)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>
+             {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+             };
+
+            var key = new SymmetricSecurityKey(
+              Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)
+            );
+
+            var credentials = new SigningCredentials(
+                key,
+                SecurityAlgorithms.HmacSha256
+            );
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(
+                    Convert.ToDouble(_configuration["Jwt:DurationInMinutes"])
+                ),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+    }
+
+}
---
+++ BlogPlatform.API/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using BlogPlatform.API.DTO.Auth;
+
+namespace BlogPlatform.API.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
+
+        Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    }
+}
