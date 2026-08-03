2c91e0ef9b5a56c32f94d8adb75cc3fd6ed6cdd7
Hamza4912 <hamzajamil1046@gmail.com>
2026-08-03 17:53:13 +0500

    Implement user profile management

---
+++ BlogPlatform.API/DTO/User/UpdateProfileDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogPlatform.API.DTOs.User
+{
+    public class UpdateProfileDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
